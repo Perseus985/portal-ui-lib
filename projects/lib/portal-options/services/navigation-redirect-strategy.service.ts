7f5891528f6b0a4344f73a8e83fc20d3f69fd52f
Andrian Krautsou <117839500+Sobyt483@users.noreply.github.com>
2026-02-10 20:04:55 +0400

    Navigation strategy (#244)
    * feat: add navigation redirect impl
    
    * feat: address pr comments
    
    ---------
    
    Co-authored-by: Grzegorz Krajniak <gkrajniak@gmail.com>

---
+++ projects/lib/portal-options/services/navigation-redirect-strategy.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { LocalStorageKeys, NavigationRedirectStrategy } from '@openmfp/portal-ui-lib';
+
+@Injectable({ providedIn: 'root' })
+export class NavigationRedirectStrategyServiceImpl implements NavigationRedirectStrategy {
+  getRedirectUrl(): string {
+    return localStorage.getItem(LocalStorageKeys.LAST_NAVIGATION_URL) || '';
+  }
+
+  saveRedirectUrl(url: string): void {
+    if (url.startsWith('/error')) {
+      return;
+    }
+
+    localStorage.setItem(LocalStorageKeys.LAST_NAVIGATION_URL, url);
+  }
+
+  clearRedirectUrl(): void {
+    localStorage.removeItem(LocalStorageKeys.LAST_NAVIGATION_URL);
+  }
+}
